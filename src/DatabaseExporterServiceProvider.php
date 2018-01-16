d02b0fed555cf8024ee49b4115b5208c68b2c6f8
KainnanP <kainnan@pipedigital.com>
2018-01-16 09:49:09 -0200

    HELOO WORDINHO

---
+++ src/DatabaseExporterServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace DatabaseExporter;
+
+use Illuminate\Events\Dispatcher;
+use Illuminate\Support\ServiceProvider;
+
+class DatabaseExporterServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
